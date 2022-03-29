039c97e37f47d3133cbe867aa92326d3a3b972da
Mary_Varf <bma1989@yandex.ru>
2022-03-29 08:24:07 +0200

    add design page

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -4,7 +4,7 @@ const bodyStyles = window.getComputedStyle(document.body);
 const gap = parseInt(bodyStyles.getPropertyValue('--grid-gap'));
 
 const portSlider = document.querySelector('.portfolio-section__items');
-const relatedSlider = document.querySelector('.related-projects__items');
+const relatedSlider = document.querySelector('.related-products__items');
 
 if (portSlider) {
   const portfolioSlider = new Swiper(portSlider, {
@@ -60,6 +60,69 @@ if (portSlider) {
     const nextNextActiveSlide = nextActiveSlide.nextElementSibling;
 
     document.querySelectorAll('.portfolio-section__items .swiper-slide').forEach(el => {
+      el.classList.remove('slider-visible');
+    });
+
+    activeSlide.classList.add('slider-visible');
+    nextActiveSlide.classList.add('slider-visible');
+    nextNextActiveSlide.classList.add('slider-visible');
+  });
+}
+
+if (relatedSlider) {
+  const relatedProdSlider = new Swiper(relatedSlider, {
+    slidesPerView: 1,
+    spaceBetween: gap,
+    on: {
+      init: function () {
+        const activeSlide = portSlider.querySelector('.swiper-slide-active');
+
+        const nextActiveSlide = activeSlide.nextElementSibling;
+
+        const nextNextActiveSlide = nextActiveSlide.nextElementSibling;
+
+        activeSlide.classList.add('slider-visible');
+        nextActiveSlide.classList.add('slider-visible');
+        nextNextActiveSlide.classList.add('slider-visible');
+      },
+    },
+    navigation: {
+      nextEl: '.related-products__next',
+      prevEl: '.related-products__prev',
+    },
+    breakpoints: {
+      576: {
+        slidesPerView: 2
+      },
+      768: {
+        slidesPerView: 3,
+      }
+    }
+  });
+
+  document.querySelector('.related-products__prev').addEventListener('click', () => {
+    const activeSlide = portSlider.querySelector('.swiper-slide-next');
+
+    document.querySelectorAll('.related-products__items .swiper-slide').forEach(el => {
+      el.classList.remove('slider-visible');
+    });
+
+    if (activeSlide.previousElementSibling) {
+      const nextActiveSlide = activeSlide.previousElementSibling;
+      activeSlide.classList.add('slider-visible');
+      nextActiveSlide.classList.add('slider-visible');
+      activeSlide.nextElementSibling.classList.add('slider-visible');
+    }
+  });
+
+  document.querySelector('.related-products__next').addEventListener('click', () => {
+    const activeSlide = portSlider.querySelector('.swiper-slide-active');
+
+    const nextActiveSlide = activeSlide.nextElementSibling;
+
+    const nextNextActiveSlide = nextActiveSlide.nextElementSibling;
+
+    document.querySelectorAll('.related-products__items .swiper-slide').forEach(el => {
       el.classList.remove('slider-visible');
     });
 
